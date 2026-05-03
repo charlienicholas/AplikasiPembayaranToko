2774207c66a9dae03ff80d909d26bd9b397a090c
charlienicholas <kangwibu129@gmail.com>
2026-05-04 00:03:02 +0700

    feat: menambahkan Produk.java di app.model

---
+++ src/app/model/Produk.java
@@ -0,0 +1,16 @@
+package app.model;
+
+public class Produk {
+    
+    private String nama;
+    private double harga;
+
+    public String getNama(){return nama;}
+
+    public double getHarga(){return harga;}
+
+    public void setNama(String nama){this.nama = nama;}
+
+    public void setHarga(double harga){this.harga = harga;}
+
+}
