b9ff7913b858fdd5dd32d28d50e27b565dab3f3a
charlienicholas <kangwibu129@gmail.com>
2026-05-04 08:54:24 +0700

    feat: menambahkan class service Kasir.java

---
+++ src/app/service/Kasir.java
@@ -0,0 +1,29 @@
+package app.service;
+
+import java.util.ArrayList;
+
+import app.model.Produk;
+import app.payment.MetodePembayaran;
+
+public class Kasir {
+
+    private double total;
+    private ArrayList<Produk> belanja = new ArrayList<>();
+
+    public void tambahProduk(Produk produk){
+        this.belanja.add(produk);
+        System.out.println(produk.getNama() + " berhasil ditambahkan ke keranjang.");
+    }
+
+    public double hitungTotal(){
+        for (int i = 0 ; i <= belanja.size() ; i++){
+            total += this.belanja.get(i).getHarga();
+        }
+        return total;
+    }
+
+    public void lakukanPembayaran(MetodePembayaran mp){
+        mp.prosesBayar(this.total);
+    }
+
+}
